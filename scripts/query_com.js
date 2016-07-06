84c15212837afe88242bf3c6be4da20676fe8e5a
nike4859 <nike4859@gmail.com>
2016-07-06 21:34:51 +0800

    選單顏色調整

--- scripts/query_com.js
+++ scripts/query_com.js
@@ -549,10 +549,10 @@ var QueryNav = React.createClass({
 		    $('.navbar-toggle').click() //bootstrap 3.x by Richard
 		});
         return (
-		    <nav className="navbar navbar-inverse navbar-fixed-top">
+		    <nav className="navbar navbar-fixed-top">
 		        <div className="container">
 		            <div className="navbar-header">
-		                <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+		                <button type="button" className="navbar-toggle collapsed btn" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
 		                    <span className="sr-only">Toggle navigation</span>
 		                    <span className="icon-bar"></span>
 		                    <span className="icon-bar"></span>
